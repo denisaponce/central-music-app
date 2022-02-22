a3e05f207c13c66618214455650f59666a2234e4
Denisa Ponce <denisaponce2012@hotmail.com>
2022-02-21 21:34:24 -0300

    feat: redux finished

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { setOffDarkMode, setOnDarkMode } from './redux/actions/themeActions';
+
+const App = () => {
+ 
+  return (
+    <>
+      <h1>Central Music App</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/redux/actions/actionsTypes.js
@@ -0,0 +1,5 @@
+const actionsTypes = {
+  // ON_DARK_MODE: 'ON_DARK_MODE',
+  // OFF_DARK_MODE: 'OFF_DARK_MODE',
+}
+export default actionsTypes;
