48f0ffdc78c08260893e1342057f59c794762fad
facu410 <facundito410@gmail.com>
2022-02-23 19:35:35 -0300

    navbar and components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
+import Navbar from './pages/navbar'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
+    <Navbar/>
     <App />
   </Provider>,
   document.getElementById('root')
