ab3bd3e2d29fcb6262e36fd49d7e4e053bc41d53
leandromartinez236 <martinezleandro2004@gmail.com>
2022-02-23 20:23:49 -0300

    commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Cards from '../components/Cards'
 
 const HomePage = () => {
 
   return (
     <div>
       <h1>HomePage</h1>
-      <Cards />
+      {/* <Cards /> */}
     </div>
   )
 }
