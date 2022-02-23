6ab22e295ff8c32d08d7a7ba9b90299f9a7f3bc3
leandromartinez236 <martinezleandro2004@gmail.com>
2022-02-23 20:22:51 -0300

    cards en proceso

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Cards from '../components/Cards'
 
 const HomePage = () => {
 
   return (
     <div>
       <h1>HomePage</h1>
+      <Cards />
     </div>
   )
 }
