cc68041f3a124c8f929e9582c1492cc936bf48d9
leandromartinez236 <martinezleandro2004@gmail.com>
2022-02-23 21:24:30 -0300

    cards

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Cards from '../components/Cards'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import {guitarras} from '../data/newProducts';
 
 const HomePage = () => {
-
   return (
-    <div>
-      <h1>HomePage</h1>
-      {/* <Cards /> */}
-    </div>
+    <ChakraProvider>
+      {/* <h1>HomePage</h1> */}
+      <Cards  product={guitarras}/>
+    </ChakraProvider>
   )
 }
 
-export default HomePage
+export default HomePage;
